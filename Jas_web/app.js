e063b8f0b17048429599fffc5a9488ac768ae8ab
JasmineSun33 <jasmine.jysun@gmail.com>
2020-06-24 20:15:32 -0400

    set up server

---
+++ Jas_web/app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+// app.set("view engine", "ejs");
+
+var courses = {
+    'CSC265': {
+        "wordcloud_path":"Wordcloud_image/CSC265.txt", 
+        "score": 0.07414503502942925, 
+        "top_reviews":["this course is very challanging..."]
+    },
+    'ECO101': {
+        "wordcloud_path":"Wordcloud_image/ECO101.txt", 
+        "score": 0.23414503502942925, 
+        "top_reviews":["very easy hahahahah"]
+    },
+};
+    
+app.get("/", function(req, res){
+    res.sendFile(__dirname+'/landing.html');
+});
+
+app.get("/result", function(req, res){
+    res.sendFile(__dirname+'/result.html');
+});
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("===========================");
+  console.log("       SERVER STARTED      ");
+  console.log("===========================");
+});
